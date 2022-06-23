78a036578223fc1e5df5ddccfd0733ea63ac48b2
zhuchuanyong <zhu1251637837@gmail.com>
2022-06-23 17:43:40 +0800

    feat: 1and7

---
+++ 1.两数之和.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  let map =new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const item = nums[i];
+    let dif=target -item
+    if(map.has(dif)){
+      return [map.get(dif),i]
+    }else {
+      map.set(item,i)
+    }
+  }
+  return []
+};
+// @lc code=end
+
