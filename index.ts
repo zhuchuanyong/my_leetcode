0cfb104435a9ca2fcd9e97b4460df39b15e28612
zhuchuanyong <zhu1251637837@gmail.com>
2022-10-26 11:48:27 +0800

    feat: config

---
+++ index.ts
@@ -0,0 +1,12 @@
+// import { ListNode } from "./lib/index";
+// const {ListNode} = require('./lib/index')
+// const head = ListNode.create([1, 2, 3, 4, 5])
+// debugger
+// console.log('head', head)
+
+// const show=head.show();
+// debugger
+// console.log('show', show)
+// import './26.删除有序数组中的重复项.js'
+// import './27.移除元素.js'
+import './35.搜索插入位置.ts'
